091ae1c48ff2daa5f4a3e4f82f77c45fd8acdeff
Luke Berry <luke@braid.com>
2015-10-26 03:56:01 +0900

    removed unused file handler function

--- src/subtitleparty/handler.clj
+++ src/subtitleparty/handler.clj
@@ -5,9 +5,6 @@
             [clojure.java.io :as io]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
-(defn handle-file [file]
-  (println (get file :filename)))
-
 (defroutes app-routes
   (GET "/" [] "<body><form action=\"http://localhost:3000/upload\" method=\"post\" enctype=\"multipart/form-data\"><input type=\"file\" name=\"file\"></input><input type=\"submit\"></input></form></body>")
   (POST "/upload" {{{tempfile :tempfile filename :filename} :file} :params :as params}
