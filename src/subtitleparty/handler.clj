b0d7648ce15587a6dd52a75ca561c05f5b8414a0
Luke Berry <luke@braid.com>
2015-10-25 11:45:53 +0900

    basic file upload thing

--- src/subtitleparty/handler.clj
+++ src/subtitleparty/handler.clj
@@ -1,11 +1,18 @@
 (ns subtitleparty.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
+            [clojure.java.io :as io]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
+(defn handle-file [file]
+  (println (get file :filename)))
+
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/" [] "<body><form action=\"http://localhost:3000/upload\" method=\"post\" enctype=\"multipart/form-data\"><input type=\"file\" name=\"file\"></input><input type=\"submit\"></input></form></body>")
+  (POST "/upload" {{{tempfile :tempfile filename :filename} :file} :params :as params}
+    (io/copy tempfile (io/file "resources" "public" filename)) "file!")
   (route/not-found "Not Found"))
 
 (def app
-  (wrap-defaults app-routes site-defaults))
+  (wrap-defaults app-routes (assoc-in site-defaults [:security :anti-forgery] false)))
+
